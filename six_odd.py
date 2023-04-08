fa9e537283aa53dbecc48d08ab46d2081937f945
HenriqueFrancaa <127359687+HenriqueFrancaa@users.noreply.github.com>
2023-04-08 19:33:15 -0300

    the six odd

---
+++ six_odd.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+if n%2 == 0:
+    print(n + 1)
+    print(n + 3)
+    print(n + 5)
+    print(n + 7)
+    print(n + 9)
+    print(n + 11)
+
+if n%2 != 0:
+    print(n)
+    print(n + 2)
+    print(n + 4)
+    print(n + 6)
+    print(n + 8)
+    print(n + 10)
