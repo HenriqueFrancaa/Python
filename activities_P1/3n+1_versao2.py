374ef0cba17654afb72b94848bade46aaf9a72be
HenriqueFrancaa <henrique23.franca@gmail.com>
2023-05-14 11:54:11 -0300

    Questão 407 The Huxley (só utilizando o while)

--- activities_P1/3n+1_versao2.py
+++ activities_P1/3n+1_versao2.py
@@ -6,11 +6,13 @@ while True:
 
     # a questão consiste em saber qual é o maior ciclo que existe em um intervalo de dois números.
     # por exemplo se n1 = 1 e n2 = 10, concorda que existe uma distancia de 9 números até chegar o 10.
-    # então seria preciso fazer o ciclo do 2, depois o de 3 e assim por diante, e qual desses vai ter o ciclo maior? eis a questão e qual é o tamanho do ciclo? é isso que vamos descobrir.
+    # então seria preciso fazer o ciclo do 2, depois o de 3 e assim por diante, e qual desses vai ter o ciclo maior? 
+    # eis a questão e qual é o tamanho do ciclo? é isso que vamos descobrir.
 
     maior_ciclo = 0
     i = 1
-    numero = n1  # a variável número, vai ficar atualizando pra cada rodada do bloco, no caso do exemplo que eu falei, é como se ele fosse o numero 2 e depois virasse o 3 e assim por diante.
+    numero = n1  # a variável número, vai ficar atualizando pra cada rodada do bloco, no caso do exemplo que eu falei, 
+    # é como se ele fosse o numero 2 e depois virasse o 3 e assim por diante.
 
     while numero < n2:
         ciclo = 1 #ciclo está valendo 1, porque ele já está contando com o numero 1.
@@ -30,6 +32,6 @@ while True:
         if ciclo > maior_ciclo:
             maior_ciclo = ciclo  # aqui, em cada rodada de número, ele fica atualizando qual é o maior ciclo.
 
-    print(maior_ciclo)
+    print(f"{n1} {n2} {maior_ciclo}")
 
 
