c8dfea6de192e116b4ce113c09421b825e8cc221
HenriqueFrancaa <henrique23.franca@gmail.com>
2023-05-21 01:46:14 -0300

    Questão 1002 Beecrowd.py

---
+++ Beecrowd/Area of a circle.py
@@ -0,0 +1,6 @@
+R= float(input())
+n= 3.14159
+area= n * (R * R)
+print(f"A={area:.4f}")
+
+#Questão 1002 Beecrowd (:
---
+++ treino_for/.py
@@ -0,0 +1,11 @@
+teste = int(input())
+
+i = 2
+primo = 0
+while i <= teste and primo ==0:
+    numero = int(input())
+    if numero%i == 0:
+        primo = 1
+        
+
+    
