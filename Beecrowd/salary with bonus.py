184d25f8bb75d2269563c0c381ad05a72f5c0d8d
HenriqueFrancaa <henrique23.franca@gmail.com>
2023-05-21 01:48:00 -0300

    Questão 1009 Beecrowd.py

---
+++ Beecrowd/salary with bonus.py
@@ -0,0 +1,9 @@
+vendedor= input()
+salario_fixo= float(input())
+vendas_total= float(input())
+comissao= 15/100
+salario_total= (vendas_total * comissao) + salario_fixo
+resultado= salario_total
+print(f"TOTAL = R$ {resultado:.2f}")
+
+#Questão 1009 Beecrowd :)
